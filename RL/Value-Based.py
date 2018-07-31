a8b6617ed41f2f9f67754059b812c4a25487a7d3
viaeou <13018056625@163.com>
2018-07-31 16:34:34 +0800

    edit Value-Based.py

--- RL/Value-Based.py
+++ RL/Value-Based.py
@@ -126,7 +126,7 @@ plt.show()
 
 
 class Qnetwork():
-    def __init__(self):
+    def __init__(self, h_size):
         self.scalarInput = tf.placeholder(shape=[None, 21168],
                                           dtype=tf.float32)
         self.imageIn = tf.reshape(self.scalarInput, shape=[-1, 84, 84, 3])
@@ -155,3 +155,149 @@ class Qnetwork():
         self.streamA = tf.contrib.layers.flatten(self.streamAC)
         self.streamV = tf.contrib.layers.flatten(self.streamVC)
         self.AW = tf.Variable(tf.random_normal([h_size//2, env.actions]))
+        self.VW = tf.Variable(tf.random_normal([h_size//2, 1]))
+        self.Advantages = tf.matmul(self.streamA, self.AW)
+        self.Value = tf.matmul(self.streamV, self.VW)
+
+        self.Qout = self.Value + tf.subtract(self.Advantages, tf.reduce_mean(
+            self.Advantages, reduction_indices=1, keep_dims=True
+        ))
+        self.predict = tf.argmax(self.Qout)
+
+        self.targetQ = tf.placeholder(shape=[None], dtype=tf.float32)
+        self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
+        self.actions_onehot = tf.one_hot(self.actions, env.actions, dtype=tf.float32)
+        self.Q = tf.reduce_sum(tf.multiply(self.Qout, self.actions_onehot),
+                               reduction_indices=1)
+
+        self.td_error = tf.square(self.targetQ - self.Q)
+        self.loss = tf.reduce_mean(self.td_error)
+        self.trainer = tf.train.AdamOptimizer(learning_rate=0.0001)
+        self.updateModel = self.trainer.minimize(self.loss)
+
+
+class experience_buffer():
+    def __init__(self, buffer_size = 50000):
+        self.buffer = []
+        self.buffer_size = buffer_size
+
+    def add(self, experience):
+        if len(self.buffer) + len(experience) >= self.buffer_size:
+            self.buffer[0:(len(experience) + len(self.buffer)) - self.buffer_size] = []
+        self.buffer.extend(experience)
+
+    def sample(self, size):
+        return np.reshape(np.array(random.sample(self.buffer, size)), [size, 5])
+
+
+def processState(states):
+    return np.reshape(states, [21168])
+
+
+def updateTargetGraph(tfVars, tau):
+    total_vars = len(tfvars)
+    op_holder = []
+    for idx, var in enumerate(tfVars[0:total_vars//2]):
+        op_holder.append(tfVars[idx+total_vars//2].assign((var.value() * tau) +
+                                                          ((1 - tau)*tfVars[idx+total_vars//2].value())))
+
+    return op_holder
+
+
+def updateTarget(op_holder, sess):
+    for op in op_holder:
+        sess.run(op)
+
+
+batch_size = 32
+update_fre = 4
+y = .99
+startE = 1
+endE = 0.1
+anneling_steps = 10000.
+num_episodes = 10000
+pre_train_steps = 10000
+max_epLength = 50
+load_model = False
+path = './dqn'
+h_size = 512
+tau = 0.001
+
+mainQN = Qnetwork(h_size)
+targetQN = Qnetwork(h_size)
+init = tf.global_variables_initializer()
+
+trainables = tf.trainable_variables()
+targetOps = updateTargetGraph(trainables, tau)
+
+myBuffer = experience_buffer()
+e = startE
+stepDrop = (startE - endE) / anneling_steps
+
+rList = []
+total_steps = 0
+
+saver = tf.train.Saver()
+if not os.path.exists(path):
+    os.makedirs(path)
+
+with tf.Session() as sess:
+    if load_model == True:
+        print('Loading Model')
+        ckpt = tf.train.get_checkpoint_state(path)
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    sess.run(init)
+    updateTarget(targetOps, sess)
+    for i in range(num_episodes + 1):
+        episodeBuffer = experience_buffer()
+        s = env.reset()
+        s = processState(s)
+        d = False
+        rALL = 0
+        j = 0
+
+        while j < max_epLength:
+            j += 1
+            if np.random.rand(1) < e or total_steps < pre_train_steps:
+                a = np.random.rand(0 ,4)
+            else:
+                a = sess.run(mainQN.predict,
+                             feed_dict={mainQN.scalarInput: [s]})[0]
+            s1, r, d = env.step(a)
+            s1 = processState(s1)
+            total_steps += 1
+            episodeBuffer.add(np.reshape(np.reshape(np.array([s, a, r, s1, d]), [1, 5])))
+            if total_steps > pre_train_steps:
+                if e > endE:
+                    e -= stepDrop
+                if total_steps % (update_fre) == 0:
+                    trainBatch = myBuffer.sample(batch_size)
+                    A = sess.run(mainQN.predict, feed_dict={
+                        mainQN.scalarInput: np.vstack(trainBatch[:, 3])
+                    })
+                    Q = sess.run(targetQN.Qout, feed_dict={
+                        targetQN.scalarInput: np.vstack(trainBatch[:, 3])
+                    })
+                    doubleQ = Q[range(batch_size), A]
+                    targetQ = trainBatch[:, 2] + y*doubleQ
+                    _ = sess.run(mainQN.updateModel,feed_dict={
+                        mainQN.scalarInput: np.vstack(trainBatch[:, 0]),
+                        mainQN.targetQ: targetQ,
+                        mainQN.actions: trainBatch[:, 1]
+                    })
+                    updateTarget(targetOps, sess)
+            rALL += r
+            s = s1
+
+            if d == True:
+                break
+
+        myBuffer.add(episodeBuffer.buffer)
+        rList.append(rALL)
+        if i > 0 and i % 25 == 0:
+            print('episode', i, ', average reward of last 25 episodes',
+                  np.mean(rList[-25:]))
+        if i > 0 and i % 1000 == 0:
+            saver.save(sess, path+'/model-'+str(i)+'.cptk')
+            print('Save model')
+    saver.save(sess, path+'/model-'+str(-1)+'.cptk')
