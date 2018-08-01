d9224a89786738075b487d759c9adbbf2f8025ad
viaeou <13018056625@163.com>
2018-08-01 16:55:04 +0800

    transfer learning

---
+++ mofan/transfer learning.py
@@ -0,0 +1,9 @@
+# author: viaeou
+from urllib.request import urlretrieve
+import os
+import numpy as np
+import tensorflow as tf
+import skimage.io
+import skimage.transform
+import matplotlib.pyplot as plt
+
