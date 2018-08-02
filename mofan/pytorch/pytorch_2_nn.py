c9d7f929d48af248836938c501ac98a139fe8c6c
gstanding <869734632@qq.com>
2018-08-02 17:45:51 +0800

    some about pytorch

---
+++ mofan/pytorch/pytorch_2_nn.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+# @Time    : 18-8-2 上午10:25
+# @Author  : viaeou
+# @Site    : 
+# @File    : pytorch_2_nn.py
+# @Software: PyCharm
+
+
+import os
+import torch
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+dtype = torch.float
+device = torch.device('cpu')
+
+N, D_in, H, D_out = 64, 1000, 100, 10
+
+x = torch.randn(N, D_in, device=device, dtype=dtype)
+y = torch.randn(N, D_out, device=device, dtype=dtype)
+
+w1 = torch.randn(D_in, H, device=device, dtype=dtype)
+w2 = torch.randn(H, D_out, device=device, dtype=dtype)
+
+learning_rate = 1e-6
+for t in range(500):
+    h = x.mm(w1)
+    h_relu = h.clamp(min=0)
+    y_pred = h_relu.mm(w2)
+
+    loss = (y_pred - y).pow(2).sum().item()
+    print(t, loss)
+
+    # Backprop to compute gradients of w1 and w2 with respect to loss
+    grad_y_pred = 2.0 * (y_pred - y)
+    grad_w2 = h_relu.t().mm(grad_y_pred)
+    grad_h_relu = grad_y_pred.mm(w2.t())
+    grad_h = grad_h_relu.clone()
+    grad_h[h < 0] = 0
+    grad_w1 = x.t().mm(grad_h)
+
+    # Update weights using gradient descent
+    w1 -= learning_rate * grad_w1
+    w2 -= learning_rate * grad_w2
